df8433ac67f0188cdc76fcdd03d3c85451c270f0
Philip Rodrigues <philip@hypr.co.nz>
2022-05-29 14:49:03 +1200

    Fixed version detector

--- get_web_version_from_toml.sh
+++ get_web_version_from_toml.sh
@@ -1,2 +1,2 @@
-VERSION_DATA=$(cat noted2xero_web/Cargo.toml  | grep "^version" | tr " = " "\n")
+VERSION_DATA=$(cat noted2xero_web/Cargo.toml  | grep "^version" | tr " = " "\n" |  head -n 4 )
 echo "${VERSION_DATA##*$'\n'}" | sed "s/\"//g"
